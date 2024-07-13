0410c1dfe7348a7437f2954b2a19df476b952fc3
maheshgodwani <mhsmca5723@gmail.com>
2024-07-14 00:50:41 +0530

    2

---
+++ 2.kt
@@ -0,0 +1,8 @@
+// Write a program that prints your name and your college name.
+
+fun main() {
+    val name = "Mahesh Godwani"
+    val address = "Morbi"
+    println("Name: $name")
+    println("Address: $address")
+}
